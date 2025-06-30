4ec7ff0b835bdc099499270c790c614d11deae4e
Pedromoreiragit <pedromoreirat242@gmail.com>
2025-06-30 18:54:54 -0300

    feet: Add login screen

---
+++ resources/views/login.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.layout_auth')
+
+@section('title', 'Login - NeoShop')
+
+
+@section('content')
+
+    {{-- Logo --}}
+    <div class="mb-3 flex justify-center">
+        <img class="w-56" src="{{ asset('images/Logo.svg') }}" alt="Logo">
+    </div>
+
+    {{-- Login Card --}}
+    <div class="bg-dark_blue_gray p-8 rounded-lg mb-4">
+        {{-- Title  --}}
+        <div class="text-white text-center mb-10">
+            <h1 class="text-2xl font-bold mb-2">Seja Bem-Vindo de Volta</h1>
+            <span class="text-gray-500">Entre com seu e-mail e senha</span>
+        </div>
+
+
+        {{-- Form --}}
+        <div class="flex flex-col gap-10 mb-4">
+            {{-- Email  --}}
+            <div class="border-b pb-2  flex gap-2 font-semibold">
+                <i class="ri-mail-line text-white font-sm"></i>
+                <input type="text" placeholder="E-mail"
+                    class="bg-transparent placeholder-white text-white w-full focus:outline-none font-sm">
+            </div>
+
+            {{-- Password  --}}
+            <div class="border-b pb-2 flex gap-2 font-semibold">
+                <i class="ri-lock-line text-white text-sm"></i>
+                <input type="text" placeholder="Password"
+                    class="bg-transparent placeholder-white text-white w-full focus:outline-none font-sm">
+            </div>
+        </div>
+
+        {{-- Resset Password --}}
+        <div class="mb-10">
+            <h2 class="text-white font-semibold text-sm">Esqueceu sua senha?
+                <a href="" class="text-blue_purple">Redefinir Senha</a>
+            </h2>
+        </div>
+
+        {{-- Button --}}
+        <div class="text-center mb-6">
+            <button class="text-white font-semibold text-xl bg-base rounded-lg py-2 w-full hover:bg-dark">Login</button>
+        </div>
+
+        {{-- Register --}}
+        <div>
+            <h2 class="text-white font-semibold text-sm text-center">Ainda não tem registro?
+                <a href="" class="text-blue_purple">Cadastre-se</a>
+            </h2>
+        </div>
+    </div>
+
+    {{-- Terms and conditionals --}}
+    <div>
+        <h2 class="text-gray-500 font-semibold text-sm text-center">Ao continuar, você aceita nossos
+            <br> Termos de Uso e Política de Privacidade
+        </h2>
+    </div>
+
+
+@endsection
